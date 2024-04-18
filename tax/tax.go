bb84d64bb092ee6060505d6d8aeea0e6d92f87a4
bytesbanana <saharat.paynok@gmail.com>
2024-04-19 01:09:18 +0700

    get rid of temp var

--- tax/tax.go
+++ tax/tax.go
@@ -12,13 +12,11 @@ func New() TaxCalculator {
 func (t TaxCalculator) calculate(totalIncome float64) float64 {
 	income := totalIncome - PERSONAL_TAX_DEDUCTION
 
-	tax := 0.0
-
 	if income <= 150_000 {
-		tax = 0
+		return 0
 	} else if income <= 500_000 {
-		tax = (income - 150_000) * 0.1
+		return (income - 150_000) * 0.1
 	}
 
-	return tax
+	return 0
 }
