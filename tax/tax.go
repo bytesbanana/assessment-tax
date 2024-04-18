4af2d46a8fba457103ec60a4901c275d9e8a1e2c
bytesbanana <saharat.paynok@gmail.com>
2024-04-19 01:09:18 +0700

    rename incomeAfterPersonalTaxDeduction to income

--- tax/tax.go
+++ tax/tax.go
@@ -10,14 +10,14 @@ func New() TaxCalculator {
 }
 
 func (t TaxCalculator) calculate(totalIncome float64) float64 {
-	incomeAfterPersonalTaxDeduction := totalIncome - PERSONAL_TAX_DEDUCTION
+	income := totalIncome - PERSONAL_TAX_DEDUCTION
 
 	tax := 0.0
 
-	if incomeAfterPersonalTaxDeduction <= 150_000 {
+	if income <= 150_000 {
 		tax = 0
-	} else if incomeAfterPersonalTaxDeduction <= 500_000 {
-		tax = (incomeAfterPersonalTaxDeduction - 150_000) * 0.1
+	} else if income <= 500_000 {
+		tax = (income - 150_000) * 0.1
 	}
 
 	return tax
