c2d50f2fb56a9b6775fa0420cfd2c920071bf3e7
bytesbanana <saharat.paynok@gmail.com>
2024-04-19 01:39:21 +0700

    Refactor test to use test table

--- tax/tax_test.go
+++ tax/tax_test.go
@@ -2,6 +2,7 @@ package tax
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -41,163 +42,57 @@ func TestTaxCalculation(t *testing.T) {
 		}
 	})
 
-	t.Run("given total income 210,000 should return tax 0", func(t *testing.T) {
-		c, rec := setup(t, func() *http.Request {
-			reqJSON := `{
-				"totalIncome": 210000.0,
-				"wht": 0.0,
-				"allowances": [
-				  {
-					"allowanceType": "donation",
-					"amount": 0.0
-				  }
-				]
-			}`
-			return httptest.NewRequest(http.MethodPost, "/", strings.NewReader(reqJSON))
+	testCases := []struct {
+		income      float64
+		expectedTax float64
+	}{
+		{
+			income:      210_000,
+			expectedTax: 0,
+		},
+		{
+			income:      210_001,
+			expectedTax: 0.1,
+		},
+		{
+			income:      560_000,
+			expectedTax: 35_000,
+		},
+		{
+			income:      560_001,
+			expectedTax: 35_000.15,
+		},
+		{
+			income:      1_060_000,
+			expectedTax: 110_000,
+		},
+	}
+
+	for _, tc := range testCases {
+		name := fmt.Sprintf("given total income %.2f should return tax amount %.2f", tc.income, tc.expectedTax)
+		t.Run(name, func(t *testing.T) {
+
+			c, rec := setup(t, func() *http.Request {
+				reqJSON := fmt.Sprintf(`{"totalIncome": %f}`, tc.income)
+				return httptest.NewRequest(http.MethodPost, "/", strings.NewReader(reqJSON))
+			})
+
+			h := &Handler{}
+			h.CalculateTax(c)
+
+			if rec.Code != http.StatusOK {
+				t.Errorf("invalid status code: got %v want %v",
+					rec.Code, http.StatusOK)
+			}
+
+			res := &taxCalculationResponse{}
+			json.Unmarshal(rec.Body.Bytes(), res)
+
+			if res.Tax != tc.expectedTax {
+				t.Errorf("invalid tax: got %v want %v",
+					res.Tax, tc.expectedTax)
+			}
 		})
+	}
 
-		h := &Handler{}
-		h.CalculateTax(c)
-
-		if rec.Code != http.StatusOK {
-			t.Errorf("invalid status code: got %v want %v",
-				rec.Code, http.StatusOK)
-		}
-
-		res := &taxCalculationResponse{}
-		json.Unmarshal(rec.Body.Bytes(), res)
-
-		if res.Tax != 0.0 {
-			t.Errorf("invalid tax: got %v want %v",
-				res.Tax, 0.0)
-		}
-
-	})
-	t.Run("given total income 210,001 should return tax 0.1", func(t *testing.T) {
-		c, rec := setup(t, func() *http.Request {
-			reqJSON := `{
-				"totalIncome": 210001.0,
-				"wht": 0.0,
-				"allowances": [
-				  {
-					"allowanceType": "donation",
-					"amount": 0.0
-				  }
-				]
-			}`
-			return httptest.NewRequest(http.MethodPost, "/", strings.NewReader(reqJSON))
-		})
-
-		h := &Handler{}
-		h.CalculateTax(c)
-
-		if rec.Code != http.StatusOK {
-			t.Errorf("invalid status code: got %v want %v",
-				rec.Code, http.StatusOK)
-		}
-
-		res := &taxCalculationResponse{}
-		json.Unmarshal(rec.Body.Bytes(), res)
-
-		if res.Tax != 0.1 {
-			t.Errorf("invalid tax: got %v want %v",
-				res.Tax, 0.1)
-		}
-	})
-
-	t.Run("given total income 560,000 should return tax 35,000", func(t *testing.T) {
-		c, rec := setup(t, func() *http.Request {
-			reqJSON := `{
-				"totalIncome": 560000.0,
-				"wht": 0.0,
-				"allowances": [
-				  {
-					"allowanceType": "donation",
-					"amount": 0.0
-				  }
-				]
-			}`
-			return httptest.NewRequest(http.MethodPost, "/", strings.NewReader(reqJSON))
-		})
-
-		h := &Handler{}
-		h.CalculateTax(c)
-
-		if rec.Code != http.StatusOK {
-			t.Errorf("invalid status code: got %v want %v",
-				rec.Code, http.StatusOK)
-		}
-
-		res := &taxCalculationResponse{}
-		json.Unmarshal(rec.Body.Bytes(), res)
-
-		if res.Tax != 35000 {
-			t.Errorf("invalid tax: got %v want %v",
-				res.Tax, 35000)
-		}
-	})
-
-	t.Run("given total income 560,001 should return tax 35,000.15", func(t *testing.T) {
-		c, rec := setup(t, func() *http.Request {
-			reqJSON := `{
-				"totalIncome": 560001.0,
-				"wht": 0.0,
-				"allowances": [
-				  {
-					"allowanceType": "donation",
-					"amount": 0.0
-				  }
-				]
-			}`
-			return httptest.NewRequest(http.MethodPost, "/", strings.NewReader(reqJSON))
-		})
-
-		h := &Handler{}
-		h.CalculateTax(c)
-
-		if rec.Code != http.StatusOK {
-			t.Errorf("invalid status code: got %v want %v",
-				rec.Code, http.StatusOK)
-		}
-
-		res := &taxCalculationResponse{}
-		json.Unmarshal(rec.Body.Bytes(), res)
-
-		if res.Tax != 35000.15 {
-			t.Errorf("invalid tax: got %v want %v",
-				res.Tax, 35000.15)
-		}
-	})
-
-	t.Run("given total income 1,060,000 should return tax 110,000", func(t *testing.T) {
-		c, rec := setup(t, func() *http.Request {
-			reqJSON := `{
-				"totalIncome": 1060000.0,
-				"wht": 0.0,
-				"allowances": [
-				  {
-					"allowanceType": "donation",
-					"amount": 0.0
-				  }
-				]
-			}`
-			return httptest.NewRequest(http.MethodPost, "/", strings.NewReader(reqJSON))
-		})
-
-		h := &Handler{}
-		h.CalculateTax(c)
-
-		if rec.Code != http.StatusOK {
-			t.Errorf("invalid status code: got %v want %v",
-				rec.Code, http.StatusOK)
-		}
-
-		res := &taxCalculationResponse{}
-		json.Unmarshal(rec.Body.Bytes(), res)
-
-		if res.Tax != 110000 {
-			t.Errorf("invalid tax: got %v want %v",
-				res.Tax, 110000)
-		}
-	})
 }
